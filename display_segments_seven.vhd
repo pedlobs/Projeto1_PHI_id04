9d4f73d31225df612057812f10a6ae6e0cdb7295
davidpsousa <62971705+davidpinheirods@users.noreply.github.com>
2025-06-24 15:34:41 -0300

    feat: Alterações para o envio do código para uma DE1-SoC

--- display_segments_seven.vhd
+++ display_segments_seven.vhd
@@ -11,23 +11,23 @@ begin
     process(binary_input)
     begin
         case binary_input is
-            when "0000" => segments <= "1111110"; -- 0
-            when "0001" => segments <= "0110000"; -- 1
-            when "0010" => segments <= "1101101"; -- 2
-            when "0011" => segments <= "1111001"; -- 3
-            when "0100" => segments <= "0110011"; -- 4
-            when "0101" => segments <= "1011011"; -- 5
-            when "0110" => segments <= "1011111"; -- 6
-            when "0111" => segments <= "1110000"; -- 7
-            when "1000" => segments <= "1111111"; -- 8
-            when "1001" => segments <= "1111011"; -- 9
-            when "1010" => segments <= "1110111"; -- A
-            when "1011" => segments <= "0011111"; -- B
-            when "1100" => segments <= "1001110"; -- C
-            when "1101" => segments <= "0111101"; -- D
-            when "1110" => segments <= "1001111"; -- E
-            when "1111" => segments <= "1000111"; -- F
-            when others => segments <= "0000000"; -- Blank
+            when "0000" => segments <= "1000000"; -- 0
+            when "0001" => segments <= "1111001"; -- 1
+				when "0010" => segments <= "0100100"; -- 2
+            when "0011" => segments <= "0110000"; -- 3
+            when "0100" => segments <= "0011001"; -- 4
+            when "0101" => segments <= "0010010"; -- 5
+            when "0110" => segments <= "0000010"; -- 6
+            when "0111" => segments <= "1111000"; -- 7
+            when "1000" => segments <= "0000000"; -- 8
+            when "1001" => segments <= "0010000"; -- 9
+            when "1010" => segments <= "0001000"; -- A
+            when "1011" => segments <= "0000011"; -- B
+            when "1100" => segments <= "1000110"; -- C
+            when "1101" => segments <= "0100001"; -- D
+            when "1110" => segments <= "0000110"; -- E
+            when "1111" => segments <= "0001110"; -- F
+            when others => segments <= "1111111"; -- Blank
         end case;
     end process;
 end Behavioral;
